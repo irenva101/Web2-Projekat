cc6a79311d388383c67cc9e89ee412fcd966bee9
Irena Varga <irenva101@gmail.com>
2023-07-19 21:11:43 +0200

    troslojna arhitektura

---
+++ Controllers/ArtikalController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Shared.RequestModels;
+using System.Threading.Tasks;
+
+namespace WEB2_Projekat.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArtikalController : ControllerBase
+    {
+        private readonly IArtikalService _artikalService;
+
+        public ArtikalController(IArtikalService artikalService)
+        {
+            _artikalService = artikalService;
+        }
+
+        [HttpPost]
+        public async Task<int> Post(ArtikalRequestModel model)
+        {
+            return await _artikalService.Create(model);
+        }
+    }
+}
