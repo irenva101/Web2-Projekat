77036d9837587062356b73d961f03bd2de6370ac
Irena Varga <irenva101@gmail.com>
2023-07-21 13:21:30 +0200

    Gotova porudzbina(nisam jos istestirala)

--- WEB2-Projekat/Controllers/PorudzbinaController.cs
+++ WEB2-Projekat/Controllers/PorudzbinaController.cs
@@ -1,5 +1,11 @@
 ﻿using Business.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Shared.RequestModels;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using WEB2_Projekat.Models;
 
 namespace WEB2_Projekat.Controllers
 {
@@ -13,7 +19,40 @@ namespace WEB2_Projekat.Controllers
             _porudzbinaService = porudzbinaService;
         }
 
+        [HttpGet("allPorudzbine")]
+        public async Task<ICollection<Porudzbina>> Get()
+        {
+            return await _porudzbinaService.GetAllPorudzbine();
+        }
+        [HttpGet]
+        public async Task<Porudzbina> GetPorudzbina(int idPorudzbine)
+        {
+            return await _porudzbinaService.GetPorudzbina(idPorudzbine);
+        }
+        [HttpPost]
+        public async Task<Porudzbina> Post(PorudzbinaRequestModel model)
+        {
+            return await _porudzbinaService.Create(model);
+        }
+        [HttpGet("allPorudzbineKorisnika")]
+        public async Task<ICollection<Porudzbina>> GetPorudzbinaKorisnika(int idKorisnika)
+        {
+            return await _porudzbinaService.GetAllPorudzbine(idKorisnika);
+        }
+        [HttpPatch]
+        public async Task<bool> Patch(int idPorudzbine, PorudzbinaRequestModel model)
+        {
+            return await _porudzbinaService.Patch(idPorudzbine, model);
+        }
+        [HttpDelete]
+        public async Task<bool> Delete(int idPorudzbine)
+        {
+            return await _porudzbinaService.Delete(idPorudzbine);
+        }
         
-        
+
+
+
+
     }
 }
